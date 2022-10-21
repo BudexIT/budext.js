1b32df887b4ec4016c078795f2950125ff7b89ff
beProsto <lavionperavion@gmail.com>
2022-10-21 11:14:37 +0200

    ee

--- hcssml/src/main.cpp
+++ hcssml/src/main.cpp
@@ -1,7 +1,23 @@
 #include <main.hpp>
+#include <fstream>
 
 int main() {
-	
+		printf("eeeeeeeeeeeee\n");
+	std::ifstream file;
+		printf("eeeeeeeeeeeee\n");
+	std::string filename = "./index.html";
+		printf("eeeeeeeeeeeee\n");
+	// if(argc > 1) {
+	// 	filename = argv[1];
+	// }
+	file.open(filename);
+		printf("eeeeeeeeeeeee\n");
+	if(!file.is_open()) {
+		printf("Error: couldn't find file %s!\n", filename.c_str());
+	}
+
+		printf("eeeeeeeeeeeee\n");
+
 	
 	return 0;
 }
