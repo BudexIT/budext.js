89c7e88569b59365f6780a742a4411077e837117
beProsto <lavionperavion@gmail.com>
2022-10-22 16:07:39 +0200

    idk for now

--- hssml/src/main.cpp
+++ hssml/src/main.cpp
@@ -6,7 +6,7 @@
 #include <hssml/files.hpp>
 
 int main(int argc, char **argv) {
-	std::string filename = "./index.scss";
+	std::string filename = "./index.hss";
 	if(argc > 1) {
 		filename = argv[1];
 	}
@@ -17,23 +17,9 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	std::string input = file.unwrap();
-
 	std::cout << input << std::endl;
 
-	std::cout << std::endl << "> CHANGED TO <" << std::endl << std::endl;
-
-	size_t index = 0;
-	while(true) {
-		index = input.find("build", index);
-		if(index == std::string::npos) break;
-		input.replace(index, 5, "destroy");
-		index += 5;
-	}
-	
-	std::cout << input << std::endl;
-
-	auto res = hssml::file::stringWrite(filename + ".hssml_output", input);
+	auto res = hssml::file::stringWrite("./public/index.html", input);
 	if(res.error().code()) {
 		std::cout << file.error().string() << std::endl;
 		return 1;
